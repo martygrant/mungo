61f08e8e24816392b6a024bb48ed42282223cbaf
Martin Grant <martingrant@outlook.com>
2018-10-08 15:56:40 +0100

    Using uwsgamedevbot as skeleton for this.
    Only a couple commands to get started, weather and roles.

---
+++ main.py
@@ -0,0 +1,99 @@
+##### Discord bot for Glasgow Developers, originally developed by Martin Grant
+##### Created 08/10/2018
+##### Last Update 08/10/2018
+##### Version 0.1
+##### Contributors
+#
+# https://github.com/medallyon
+#
+#####
+
+import os
+import random as rand
+import sys
+from datetime import datetime, timedelta
+from time import time as timestamp
+import asyncio
+import discord
+from discord.ext import commands
+from discord.utils import get
+import requests
+
+import modules.roles
+import modules.weather
+
+REPOSITORY_URL = "https://github.com/martygrant/mungo"
+VERSION_NUMBER = os.getenv('version')
+BOT_TOKEN = os.getenv('token')
+
+
+##### [ BOT INSTANTIATION ] #####
+
+BOT = commands.Bot(description="Below is a listing for Mungo's commands. Use '!' infront of any of them to execute a command, like '!help'", command_prefix="!")
+BOT.load_extension('modules.roles')
+BOT.load_extension('modules.weather')
+
+##### [ EVENT LISTENERS ] #####
+
+@BOT.event
+async def on_ready():
+	"""The 'on_ready' event"""
+	print("Logged in as {} ({})\n------".format(BOT.user.name, BOT.user.id))
+
+@BOT.event
+async def on_member_join(member):
+	"""The 'on_member_join' event"""
+	
+	introductionsChannel = BOT.get_channel('498797757620813834')
+	welcomeRulesChannel = BOT.get_channel('498797001043869697')
+	eventsChannel = BOT.get_channel('498796966365495298')
+	junkChannel = BOT.get_channel('498856942060699669')
+
+	welcome_message = """Welcome to the **Glasgow Developers** server!
+
+Please check out {}. Visit {} to see what events are coming up! Why not introduce yourself in {}?
+
+Type `!help` for a list of my commands.
+
+Use `!role <role name>` to add a role to your account. Use `!roles` to see what roles are available. This must be done in the {} channel.
+
+""".format("{}".format(welcomeRulesChannel.mention), "{}".format(eventsChannel.mention), "{}".format(introductionsChannel.mention), "{}".format(junkChannel.mention))
+
+	# Send the welcome message to the user individually
+	await BOT.send_message(member, welcome_message)
+	# Announce a new member joining in the introductions channel
+	await BOT.send_message(introductionsChannel, "Welcome {} to the Glasgow Developers server!".format(member.mention))
+
+@BOT.event
+async def on_member_remove(member):
+	"""The 'on_member_remove' event"""
+
+	junkChannel = BOT.get_channel('498856942060699669')
+
+	await BOT.send_message(junkChannel, "User **{}** has left the server. Goodbye!".format(str(member)))
+
+
+
+##### [ BOT COMMANDS ] #####
+
+@BOT.command()
+async def say(*something):
+	"""Make Mungo say something."""
+	if something:
+		await BOT.say(" ".join(something))
+
+@BOT.command()
+async def about():
+	"""Information about the Mungo bot."""
+
+	info = "I'm Mungo, the bot for the Glasgow Developers discord server.\nNamed after St. Mungo, the founder and patron saint of Glasgow.\n"
+	info += "This is v"
+	info += VERSION_NUMBER
+	info += ". You can view and contribute to the bot, check out: "
+	info += REPOSITORY_URL
+
+	await BOT.say(info)
+
+##### [ BOT LOGIN ] #####
+
+BOT.run(BOT_TOKEN)
---
+++ modules/roles.py
@@ -0,0 +1,95 @@
+import discord
+from discord.ext import commands
+import utilities as utils
+
+
+class Roles:
+	def __init__(self, bot):
+		self.bot = bot
+		self.restrictedRoles = ['Bot', 'Moderators', '@everyone']
+
+	@commands.command(pass_context=True)
+	async def roles(self, ctx):
+		"""Print a list of all server roles."""
+		roleString = ""
+		count = 0
+
+		rolesDict = {}
+
+		for role in ctx.message.author.server.roles:
+			if role.name not in self.restrictedRoles:
+				rolesDict[role.name] = 0
+		
+		# For each server role, for each user's list of roles, add to a count if there is a user with that role
+		for role in ctx.message.author.server.roles:
+			if role.name not in self.restrictedRoles:
+				for user in ctx.message.author.server.members:
+					for userRole in user.roles:
+						if role == userRole:
+							rolesDict[role.name] += 1
+
+		# Convert dict to list so we can sort alphabetically
+		rolesList = []
+		for k, v in rolesDict.items():
+			rolesString = "`"
+			rolesString += k
+			rolesString += "` "
+			rolesString += str(v)
+			rolesString += "\n"
+			rolesList.append(rolesString)
+
+		rolesList = sorted(rolesList)
+
+		# Covnert list to string so we can display using line terminator \n
+		rolesString = ""
+		for x in rolesList:
+			rolesString += x
+
+		embed = discord.Embed(type="rich", colour=utils.generate_random_colour())
+		embed.set_author(name="Use '!role Role Name' to add a role to your profile.")
+		embed.add_field(name="Roles", value=rolesString)
+
+		await self.bot.say(embed=embed)
+
+
+	@commands.command(pass_context=True)
+	async def role(self, ctx, *arg):
+		"""Add or remove a role, e.g "!role Hobbyist" to add or remove the role "Hobbyist"."""
+		user = ctx.message.author
+		roleToAdd = ""
+		for x in arg:
+			roleToAdd += str(x)
+			roleToAdd += " "
+		roleToAdd = roleToAdd[:-1]
+
+		if roleToAdd not in self.restrictedRoles:
+			role = None
+
+			# compare role argument with server roles by checking their lower-case representations
+			for tempRole in ctx.message.author.server.roles:
+				if tempRole.name.lower() == roleToAdd.lower():
+					print("MATCH! " + tempRole.name + " " + roleToAdd)
+					role = tempRole
+					break
+
+			if role is None:
+				return await self.bot.say("That role doesn't exist.")
+
+			# we want to skip removing the specified role if we are adding one
+			dontRemove = False
+
+			if role not in user.roles:
+				await self.bot.add_roles(user, role)
+				await self.bot.say("{} role has been added to {}.".format(role, user.mention))
+				dontRemove = True
+
+			if role in user.roles and dontRemove == False:
+				await self.bot.remove_roles(user, role)
+				await self.bot.say("{} role has been removed from {}.".format(role, user.mention))
+
+		else:   
+			await self.bot.say("This role requires manual approval from an admin.")
+
+def setup(bot):
+	bot.add_cog(Roles(bot))
+	print("Roles module loaded.")
